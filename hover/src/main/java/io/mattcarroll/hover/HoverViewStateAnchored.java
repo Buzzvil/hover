083b8c94b6bdc002414e24faa9c6ef6b702885ad
SeoJungHong <hsj0223@gmail.com>
2019-02-18 15:01:05 +0900

    anchor 위치를 바꾸고 현재 선택된 section 가져오는 로직 변경

--- hover/src/main/java/io/mattcarroll/hover/HoverViewStateAnchored.java
+++ hover/src/main/java/io/mattcarroll/hover/HoverViewStateAnchored.java
@@ -1,16 +1,16 @@
 package io.mattcarroll.hover;
 
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
 public class HoverViewStateAnchored extends BaseHoverViewState {
 
     private static final String TAG = "HoverViewStateAnchored";
-    private static final int ANCHOR_TAB_X_OFFSET_IN_PX = 100;
-    private static final int ANCHOR_TAB_Y_OFFSET_IN_PX = 100;
 
     private FloatingTab mSelectedTab;
+    private HoverMenu.Section mSelectedSection;
     private Point mDock;
 
     @Override
@@ -19,13 +19,20 @@ public class HoverViewStateAnchored extends BaseHoverViewState {
         Log.d(TAG, "Taking control.");
         mHoverView.makeUntouchableInWindow();
         mHoverView.clearFocus();
+        final int pointMargin = hoverView.getContext().getResources().getDimensionPixelSize(R.dimen.hover_tab_anchor_margin);
         mDock = new Point(
-                mHoverView.mScreen.getWidth() - ANCHOR_TAB_X_OFFSET_IN_PX,
-                ANCHOR_TAB_Y_OFFSET_IN_PX
+                mHoverView.mScreen.getWidth() - pointMargin,
+                mHoverView.mScreen.getHeight() - pointMargin
         );
 
-        HoverMenu.Section section = mHoverView.mMenu.getSection(0);
-        mSelectedTab = mHoverView.mScreen.createChainedTab(section);
+        mSelectedSection = mHoverView.mMenu.getSection(mHoverView.mSelectedSectionId);
+        if (mSelectedSection == null) {
+            mSelectedSection = mHoverView.mMenu.getSection(0);
+        }
+        mSelectedTab = mHoverView.mScreen.getChainedTab(mSelectedSection.getId());
+        if (mSelectedTab == null) {
+            mSelectedTab = mHoverView.mScreen.createChainedTab(mSelectedSection);
+        }
         mSelectedTab.setDock(new PositionDock(mDock));
         mSelectedTab.dock(new Runnable() {
             @Override
